5cfe29bfd27bdcefe59b23fc18425b3f0be592be
jaymin shiroya <jaymeenshiroya399@gmail.com>
2025-01-30 17:08:35 +0530

    feat:winning logic done

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,51 +1,25 @@
-import { useState } from "react";
+export default function GameBoard({ onSelectSquare, board }) {
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard( { onSelectSquare , turns  } ) {
-
-
-    let GameBoard = initialGameBoard;
-
-    for(const turn of turns){
-        const {square , player} = turn;
-        const {row,col} = square;
-
-        GameBoard[row][col] = player; 
-    }
-
-//   const [GameBoard,setGameBoard] = useState(initialGameBoard);
-
-//   function handleSelectSquare(rowIndex, colIndex){
-//     setGameBoard((prevGameBoard) => {
-//         const updatedBoard = [...prevGameBoard.map( innerArray => [...innerArray])];
-//         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-//         return updatedBoard;
-//     });
-
-//     onSelectSquare();
-//   }
-  
-    return (
-
+  return (
     <ol className="flex flex-col h-full w-full gap-4 p-0 md:px-25 md:py-8">
-      {GameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex} className="w-full h-1/3">
           <ol className="flex w-full h-full gap-4">
-          {row.map((playerSymbol, colIndex) => (
-            <li
-              key={colIndex}
-              className="w-1/3 bg-amber-200  flex items-center justify-center rounded-md"
-                onClick={() => {(playerSymbol === null)?onSelectSquare(rowIndex,colIndex):""}}
-            >
-              <button 
-              className="text-2xl md:text-7xl font-bold ">{playerSymbol}</button>
-            </li>
-          ))}
+            {row.map((playerSymbol, colIndex) => (
+              <li
+                key={colIndex}
+                className="w-1/3 bg-amber-200  flex items-center justify-center rounded-md"
+                onClick={() => {
+                  playerSymbol === null
+                    ? onSelectSquare(rowIndex, colIndex)
+                    : "";
+                }}
+              >
+                <button className="text-2xl md:text-7xl font-bold ">
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
           </ol>
         </li>
       ))}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import GameBoard from "./GameBoard";
 import Player from "./Players";
 import Logs from "./Logs";
+import { WINNING_COMBINATIONS as winComb } from './Winning_combination';
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function derivedActivePlayer(gameTurns){
   let currentPlayer = 'X';
@@ -14,10 +21,37 @@ function derivedActivePlayer(gameTurns){
 
 }
 
-
 export default function Home() {
   const [gameTurns, setGameTurns] = useState([]);
   let activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for(const turn of gameTurns){
+
+      const {square , player} = turn;
+      const {row,col} = square;
+
+      gameBoard[row][col] = player;
+
+      console.log("win")
+  }
+  
+  let winner;
+
+  for(const combination of winComb){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -70,9 +104,10 @@ export default function Home() {
             
             <div className="h-4/5 w-full md:w-1/2  mt-2 bg-indigo-500 flex flex-col gap-4 
             justify-center items-center rounded-md shadow-xl p-10 md:p-0">
+              {winner && <p>You won, {winner}</p>}
               <GameBoard
                 onSelectSquare={handleSelectSquare}
-                turns={gameTurns}
+                board={gameBoard}
               />
             </div>
             <ol className="h-1/5 w-full md:flex justify-center items-center text-center md:w-2/3 mt-2 gap-4 sm:hidden">
